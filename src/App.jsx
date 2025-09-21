2ad503910efb0b9d2270483584591447eff4c1a3
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-09-22 03:06:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './Counter'
+
+function App() {
+
+  return (
+    <>
+      <h1>Counter React Web App</h1>
+      <Counter></Counter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Counter.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const plusHandle = () => {
+        const increse = count + 1;
+        setCount(increse);
+    }
+
+    const minusHandle = () => {
+        const minus = count - 1;
+        if (minus == 0) {
+            setCount(minus);
+        }
+    }
+
+    const resetHandle = () => {
+        const resetBtn = 0;
+        setCount(resetBtn)
+    }
+
+    return (
+        <div className="">
+            <h2>Count : {count}</h2>
+            <button onClick={plusHandle}>+</button>
+            <button onClick={minusHandle}>-</button>
+            <button onClick={resetHandle}>Reset</button>
+        </div>
+    )
+}
